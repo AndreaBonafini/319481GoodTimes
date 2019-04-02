57147532795b4c40c50b56b9a7d89a280846d8e2
AndreaBonafini <47528585+AndreaBonafini@users.noreply.github.com>
2019-04-02 19:58:02 -0400

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+/*Andrea Bonafini
+ * March 27,2019
+ * COnverter of time through the cities
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,8 +27,8 @@ namespace U2AndreaGoodTimes
         public MainWindow()
         {
             InitializeComponent();
-        }
-
+        }            
+        
         private void btnTime_Click(object sender, RoutedEventArgs e)
         {
             string TextEntered = txtTime.Text;
@@ -37,32 +41,47 @@ namespace U2AndreaGoodTimes
             int Tor = Time;
             int Hal = Time + 100;
             int STJ = Time + 130;
-
+            
+            if (Vic % 100 > 59) Vic = Vic + 40;
+            /*in case time is for example 2070(which is impossible),
+            it converts it into the next hour by adding 40
+            (removing 100 it goes to 2170 and then removing 60 for the hour)
+            */
             if (Vic <= 0)
             {
                 Vic += 2400;
             }
+            
+            if (Ed % 100 > 59) Ed = Ed + 40;
             if (Ed <= 0)
             {
                 Ed += 2400;
             }
+            
+            if (Win % 100 > 59) Win = Win + 40;
             if (Win <= 0)
             {
                 Win += 2400;
             }
+            
+            if (Tor % 100 > 59) Tor = Tor + 40;
             if (Tor <= 0)
             {
                 Tor += 2400;
             }
+            
+            if (Hal % 100 > 59) Hal = Hal + 40;
             if (Hal >= 2400)
             {
                 Hal -= 2400;
             }
+            
+            if (STJ % 100 > 59) STJ = STJ + 40;
             if (STJ >= 2400)
             {
                 STJ -= 2400;
             }
-
+            
             lblOutput.Content += Time + " in Ottawa" + "\r" + "\n";
             lblOutput.Content += Vic + " in Victoria" + "\r" + "\n";
             lblOutput.Content += Ed + " in Edmonton" + "\r" + "\n";
@@ -74,4 +93,5 @@ namespace U2AndreaGoodTimes
         }
     }
 }
+
 
